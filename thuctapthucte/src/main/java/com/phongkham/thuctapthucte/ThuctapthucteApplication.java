3315ed96bba72f5e9ede29abbd8bbd503e347ffa
8841duyen <myduyen160903@gmail.com>
2025-07-08 09:57:12 +0700

    Initial commit

---
+++ thuctapthucte/src/main/java/com/phongkham/thuctapthucte/ThuctapthucteApplication.java
@@ -0,0 +1,13 @@
+package com.phongkham.thuctapthucte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuctapthucteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThuctapthucteApplication.class, args);
+	}
+
+}
---
+++ thuctapthucte/src/test/java/com/phongkham/thuctapthucte/ThuctapthucteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phongkham.thuctapthucte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuctapthucteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
